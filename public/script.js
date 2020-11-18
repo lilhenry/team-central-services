b0e0e1936802e3710987fe82f177b60e0fa37888
arionaskins11 <askins1@github.com>
2020-11-17 19:26:30 -0500

    ++ api

--- public/script.js
+++ public/script.js
@@ -1,18 +1,21 @@
-while (true) {
+document.body.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const div = $(e.target).serializeArray();
   fetch('/api', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(form)
+    body: JSON.stringify(div)
   }) 
     .then((fromServer) => fromServer.json())
     .then((jsonFromServer) => {
-      const API = document.createElement('div');
+      const API = document.querySelector('div');
       API.innerHTML = `<h2>What we have</h2> <br />${JSON.stringify(jsonFromServer)}<br /><br />`;
-      $('body').append(API);
+      console.log(jsonFromServer);
+      console.log(API);
     })
     .catch((err) => {
       console.log(err);
     });
-}
+});
