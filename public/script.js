bdab6d1596f5f41d1de5fee6d14ba69123cea7fc
lilhenry <lillian.f.henry99@gmail.com>
2020-11-29 22:25:04 -0500

    submit button scripting
    changed 'generate' button type to submit, added submit event listener that sends data to a function to generate chart

--- public/script.js
+++ public/script.js
@@ -1,5 +1,32 @@
-const generatechart= document.querySelector('.generate');
+/* const generatechart= document.querySelector('.generate');
 
 generatechart.onclick = function() {
     console.log('generate was clicked! now get data in here')
+} */
+
+const { json } = require("express");
+
+// generate bar chart
+function generatechart(jsonfromserver) {
+    console.log('in generatechart function, data has presumably been passed in:');
+    console.log(jsonfromserver);
 }
+
+document.body.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const form = $(e.target).serializeArray();
+
+    fetch('/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(form)
+    })
+    .then((datafromserver) => datafromserver.json())
+    .then((jsonfromserver) => generatechart(jsonfromserver))
+    .catch((err) => {
+        console.log(err);
+    });
+});
