870998551db9e61e76c5af9b65d46097346b9acc
arionaskins11 <askins1@github.com>
2020-11-29 22:48:44 -0500

    api work

--- public/script.js
+++ public/script.js
@@ -1,20 +1,36 @@
-$('#breakdown').on('click', async (e) => {
+function prepareData(serverJson) {
+  for (let obj = 0; obj < serverJson.length; obj += 1) { 
+    const payee = serverJson[obj].payee_name;
+    const agency = serverJson[obj].agency;
+    const zip = serverJson[obj].zip_code;
+    const amount = serverJson[obj].amount;
+    const description = serverJson[obj].payment_description;
+
+    const API = document.createElement('div');
+    API.innerHTML = `<h4>${payee}</h4>
+                      <p>${agency}</p>
+                      <p>${zip}</p>
+                      <p>${amount}</p> 
+                      <p>${description}</p>`;
+    $('.container').append(API);
+    document.body.style.backgroundColor = '#f3f3f3';
+  }
+}
+
+$('#generate').on('click', async (e) => {
+  console.log('generate');
   e.preventDefault();
   const div = $(e.target).serializeArray();
-  fetch('http://localhost:3000/api', {
+  fetch('http://localhost:3000/api', { 
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(div)
+    body: JSON.stringify(div),
   }) 
     .then((fromServer) => fromServer.json())
-    .then((jsonFromServer) => {
-      const API = document.createElement('div');
-      API.innerHTML = `<h2>What we have</h2> <br />${JSON.stringify(jsonFromServer)}<br /><br />`;
-      $('.container').append(API);
-    })
+    .then((jsonFromServer) => prepareData(jsonFromServer))
     .catch((err) => {
       console.log(err);
     });
-});
+});    
