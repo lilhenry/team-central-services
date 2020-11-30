e2f0d44cc456a82eeca5a8f062baae0a17e24c08
arionaskins11 <askins1@github.com>
2020-11-30 06:31:11 -0500

    ++ createSearch fxn

--- public/script.js
+++ public/script.js
@@ -1,5 +1,32 @@
+function range(int) {
+  const arr = [];
+  for (let num = 0; num < int; num += 1) {
+    arr.push(num);
+  }
+  return arr;
+}
+
+function getRandomIntInclusive(min, max) {
+  const min1 = Math.ceil(min);
+  const max1 = Math.floor(max);
+  return Math.floor(Math.random() * (max1 - min1 + 1) + min1); // The maximum is inclusive and the minimum is inclusive
+}
+
+// get and produce search results
+function createSearch(term, data) {
+  $('#search').on('click', async (e) => {
+    e.preventDefault();
+    // return search results list
+  });
+}
+
 // get json data and return bar chart data list
-function prepareData(serverJson) {
+function prepareData(serverJson) { // process your restaurants here!
+  createSearch(serverJson); // search button event listener
+  const have = document.createElement('p');
+  have.className = 'title is-uppercase is-centered';
+  have.textContent = 'Our data: ';
+  $('.container').append(have);
   for (let obj = 0; obj < serverJson.length; obj += 1) { 
     const payee = serverJson[obj].payee_name;
     const agency = serverJson[obj].agency;
@@ -9,12 +36,14 @@ function prepareData(serverJson) {
 
     // div append just to see/check api data is present
     const API = document.createElement('div');
-    API.innerHTML = (`<h4>${payee}</h4>
-                      <p>${agency}</p>
-                      <p>${zip}</p>
-                      <p>${amount}</p> 
-                      <p>${description}</p>`);
-    $('.section').append(API);
+    API.innerHTML = `<h4><span class='is-size-4'>payee:  </span>${payee}</h4>
+                      <p><span class='is-size-4'>agency:  </span>${agency}</p>
+                      <p><span class='is-size-4'>zip:  </span>${zip}</p>
+                      <p><span class='is-size-4'>amount:  </span>${amount}</p> 
+                      <p><span class='is-size-4'>description:  </span>${description}</p>
+                      <p>----------------</p>
+                      <br>`;
+    $('.container').append(API);
   }
 }
 
@@ -23,14 +52,8 @@ function generateChart(chartData) {
 
 }
 
-// get and produce search results
-function createSearch(term, data) {
-
-}
-
 // generate button click -- run server
 $('#generate').on('click', async (e) => {
-  console.log('generate');
   e.preventDefault();
   const div = $(e.target).serializeArray();
   fetch('/api', { 
@@ -43,6 +66,6 @@ $('#generate').on('click', async (e) => {
     .then((fromServer) => fromServer.json())
     .then((jsonFromServer) => prepareData(jsonFromServer))
     .catch((err) => {
-      console.log(err);
+      console.log(err, 'error');
     });
-});
+});    
