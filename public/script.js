8fb413c08565a58110db08dc17e8f144c0b1b6e9
lilhenry <lillian.f.henry99@gmail.com>
2020-12-03 15:03:02 -0500

    created main thread, moved old process into new file

--- public/script.js
+++ public/script.js
@@ -1,114 +1,50 @@
-// this function will eventually create the divs based on the returned array
-async function createSearch() { // this will take the array with selected attribute as a parameter 
-  // iterates over array and creates a div to store each result
-}
-
-// this function creates the chart container using the returned x and y values array
-async function generateChart() { // this will take the array with selected attribute as a parameter 
-  // , the {x: attribute, y: value} for canvasJS (similar to the makeOptions fxn)
-  // choose canvasJS colors for the bars
-  const attribute = document.querySelector('input[name="chart-list"]:checked').value;
-  console.log('attribute post', attribute);
-}
-
-// this is called to check if the search button is clicked (it creates the event listener for the search button)
-async function searchButtonClicked(serverJson) { // takes in json data from server as parameter
-  // this is where the .filter() and more will go
-  $('#search-btn').on('click', (e) => { // this line makes the function run only IF the search button is clicked
-    e.preventDefault();
-
-    // the rest for now is just for testing -- it creates divs to show all our data
-    const have = document.createElement('p');
-    have.className = 'is-size-2 is-uppercase is-centered';
-    have.textContent = 'Our data: ';
-
-    const brkdwn = document.querySelector('#breakdown');
-    brkdwn.innerHTML = 'SEARCH';
-
-    $('.search-results').append(have);
-    for (let obj = 0; obj < serverJson.length; obj += 1) { 
-      const payee = serverJson[obj].payee_name;
-      const agency = serverJson[obj].agency;
-      const zip = serverJson[obj].zip_code;
-      const amount = serverJson[obj].amount;
-      const description = serverJson[obj].payment_description;
-
-      // div append just to see/check api data is present
-      const API = document.createElement('div');
-      API.innerHTML = `<h4><span class='is-size-4'>payee:  </span>${payee}</h4>
-                        <p><span class='is-size-4'>agency:  </span>${agency}</p>
-                        <p><span class='is-size-4'>zip:  </span>${zip}</p>
-                        <p><span class='is-size-4'>amount:  </span>${amount}</p>
-                        <p><span class='is-size-4'>description:  </span>${description}</p>
-                        <p>----------------</p>
-                        <br>`;
-      $('.search-results').append(API);
-    }
-    createSearch(); // creates search results divs, takes in array (.filter fxn) from search bttn as parameter
-    const searchquery = document.querySelector('#search-btn').value;
-    console.log('search query post ', searchquery);
-  });
-}
-
-// this is called to check if the generate button is clicked (it creates the event listener for the generate button)
-async function generateButtonClicked(serverJson) { // takes in json data from server as parameter
-  // this  is where the .reduce() will go
-  $('#generate').on('click', (e) => {
-    e.preventDefault();
-
-    const gen = document.querySelector('#breakdown');
-    gen.innerHTML = 'GENERATE WAS CLICKED!';
-
-    // render a bar chart
-    // returns the array with the attribute and count
-    generateChart(); // takes in array (.reduce fxn) from button and render chart container
-  });
-}
-
-function main(jsonFromServer) {
-
-  // radio button attribute value
-  // const attribute = document.querySelector('input[name="chart-list"]:checked').value;
-  // console.log('attribute ', attribute);
-
-  // search bar value
-  // const searchquery = document.querySelector('#search').value;
-  // console.log('search query ', searchquery);
-
-  // creates button listeners and runs fxn with selected/entered data attribute
-  generateButtonClicked(jsonFromServer); // runs once generate button is clicked
-  searchButtonClicked(jsonFromServer); // runs once search button is clicked
-}
-
-// runs on window load: gets api data from server
-$(window).on('load', async (e) => {
-  e.preventDefault();
-  const div = $(e.target).serializeArray();
-
-  fetch('/api', { 
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(div),
-  }) 
-    .then((fromServer) => fromServer.json())
-    .then((jsonFromServer) => main(jsonFromServer))
-    .catch((err) => {
-      console.log(err, 'error');
-    });
-});
-
-// app token from pg county (1000 records issue)
-//     hit pg county when server turns on
-//     
-
 // main thread
-//      fetch await
-//      to json
-//      event listener for generate button click
-//      event listener for search button click
+async function mainthread() {
 
-// presentation: note difficulties with api records
+  // data retrieval
+  const data = await fetch('https://data.princegeorgescountymd.gov/resource/uh6s-izyj.json');
+  const json = await data.json();
 
-// https://dev.socrata.com/foundry/data.princegeorgescountymd.gov/p32t-azw8
+  // *********************
+  // generate button click
+  const generatebutton = document.querySelector('#generate');
+  generatebutton.addEventListener('click', (e) => {
+
+    // prevent default
+    e.preventDefault();
+
+    // debugging
+    const outputheader = document.querySelector('#breakdown');
+    outputheader.innerHTML = 'GENERATE WAS CLICKED';
+
+    // radio button value
+    const attribute = document.querySelector('input[name="chart-list"]:checked').value;
+    console.log('attribute ', attribute);
+  });
+
+  // *******************
+  // search button click
+  const searchbutton = document.querySelector('#search-btn');
+  searchbutton.addEventListener('click', (e) => {
+
+    // prevent default
+    e.preventDefault();
+
+    // debugging
+    const outputheader = document.querySelector('#breakdown');
+    outputheader.innerHTML = 'SEARCH WAS CLICKED';
+
+    // search bar value, debugging
+    const searchquery = document.querySelector('#search').value;
+    console.log('search query = ', searchquery);
+  });
+}
+
+window.onload = mainthread;
+
+
+// NOTES & TO DO
+//   - app token from pg county (1000 records issue)
+//   - hit pg county when server turns on
+//   - presentation: note difficulties with api records
+//   - https://dev.socrata.com/foundry/data.princegeorgescountymd.gov/p32t-azw8
